a20a014211a1880760aa69f5b6b78d798dcb8bb1
Raymond Harvey <raymh2002@aol.com>
2018-01-17 22:46:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,26 @@ function sayHiToGrandma(string) {
   if (string === shout(string))
     return "YES INDEED!"
   else if (string === whisper(string))
+    return "function shout(string) {
+return string.toUpperCase
+}
+
+function whisper(string) {
+return string.toLowerCase
+}
+
+function logShout(string) {
+console.log(shout(string))
+}
+
+function logWhisper(string) {
+console.log(whisper(string))
+}
+
+function sayHiToGrandma(string) {
+  if (string === shout(string))
+    return "YES INDEED!"
+  else if (string === whisper(string))
     return ""
+}
 }
